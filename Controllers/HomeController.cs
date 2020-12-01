1a436ac591d86e28af6d3a3ac2b9a87a402e2782
Akash Patel <36096141+a1k2a3s4h5@users.noreply.github.com>
2020-12-01 14:15:29 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using source_control_assignment1.Models;
+
+namespace source_control_assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("user")]
+        public IActionResult UserInfo(){
+            return View();
+        }
+
+        [Route("user")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UserInfo(UserInfoViewModel userModel){
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
