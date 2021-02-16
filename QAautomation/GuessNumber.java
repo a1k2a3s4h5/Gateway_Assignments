dc89f58618637c189a5859d3af09ce633a8595df
Akash Patel <36096141+a1k2a3s4h5@users.noreply.github.com>
2021-02-16 11:48:09 +0530

    Version 4 of same program

--- QAautomation/GuessNumber.java
+++ QAautomation/GuessNumber.java
@@ -7,65 +7,71 @@ public class GuessNumber {
 	 * value within 10 interval and if he will get success then system asks that he
 	 * want to play again or not if he want to play than this process will restart
 	 * and if not than exit. Created By :Akash Patel Created on:8-02-2021 Updated
-	 * on:Version 2 10-02-2021 Updated on:Version 3 15-02-2021
+	 * on:Version 2 10-02-2021 Updated on:Version 3 15-02-2021 Version 3 16-02-2021
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random range = new Random();
-		int maxinput = 100;
-		int mininput = 1;
-		String playagainvalue;
+		int maxInput = 100;
+		int minInput = 1;
+		boolean getValue;
+		String playAgainValue;
 
 		do {
-			int a = range.nextInt(maxinput - mininput) + mininput;
+			int computerValue = range.nextInt(maxInput - minInput) + minInput;
 			System.out.println("Enter value1 and value2 between 1 to 100.");
-			System.out
-					.println("Sum of your values is compared with computer values so enter your value accordingly..\n");
+			System.out.println("Sum of your values is compared with computer values so enter your value accordingly..\n");
 			Scanner s = new Scanner(System.in);
-			CompareNumber(a);
-			System.out.println("Do you want to play again?? Yes / No : ");
-			playagainvalue = s.next();
-
-		} while (playagainvalue.equalsIgnoreCase("y"));
+			getValue = compareNumber(computerValue);
+			if(getValue) {
+				System.out.println("Your 10 trials are over.");
+				System.out.println("Do you want to start again?? Yes / No : ");
+				playAgainValue = s.next();
+			}else {
+				System.out.println("You won the game.");
+				System.out.println("Do you want to play again?? Yes / No : ");
+				playAgainValue = s.next();
+			}
+			
+		} while (playAgainValue.equalsIgnoreCase("y"));
 		System.out.println("You are done with this game pleas exit it.");
 	}
 
-	static void CompareNumber(int a) {
-		int i = 0;
-		int uservalue1;
-		int uservalue2;
-		int Sumofuserinput;
+	static boolean compareNumber(int computerValue) {
+		int trials = 0;
+		int userValue1;
+		int userValue2;
+		int sumOfUserInput;
 		boolean value = false;
-		System.out.println(a);
+		System.out.println(computerValue);
 		Scanner s = new Scanner(System.in);
-		for (i = 0; i < 10; i++) {
+		for (trials = 0; trials < 10; trials++) {
 
 			System.out.println("Please enter your value 1 : ");
-			uservalue1 = s.nextInt();
+			userValue1 = s.nextInt();
 			System.out.println("Please enter your value 2 : ");
-			uservalue2 = s.nextInt();
-			Sumofuserinput = uservalue1 + uservalue2;
-			System.out.println("Sum of your values : " + Sumofuserinput);
-			if (Sumofuserinput > 100) {
-				System.out.println("Enter valid input. You are on " + (i + 1) + " trial.");
-			} else if (Sumofuserinput < 0) {
-				System.out.println("Enter valid input. You are on " + (i + 1) + " trial.");
-			} else if (Sumofuserinput == a) {
+			userValue2 = s.nextInt();
+			sumOfUserInput = userValue1 + userValue2;
+			System.out.println("Sum of your values : " + sumOfUserInput);
+			if (sumOfUserInput > 100 || sumOfUserInput < 0) {
+				System.out.println("Enter valid input. You are on " + (trials + 1) + " trial.");
+			} else if (sumOfUserInput == computerValue) {
 				System.out.println("You guess as same as compiler.");
-				System.out.println("You guess on " + (i + 1) + " trial.");
+				System.out.println("You guess on " + (trials + 1) + " trial.");
 				break;
-			} else if (Sumofuserinput > a) {
+			} else if (sumOfUserInput > computerValue) {
 				System.out.println("Sum of both values are higher than compiler value.");
-				System.out.println("You are on " + (i + 1) + " trial.");
+				System.out.println("You are on " + (trials + 1) + " trial.");
 			} else {
 				System.out.println("Sum of both values are lower than compiler value.");
-				System.out.println("You are on " + (i + 1) + " trial.");
+				System.out.println("You are on " + (trials + 1) + " trial.");
 			}
 			value = true;
 		}
-		if (value) {
+		return value;
+		/*if (value) {
 			System.out.println("Your 10 trials are over.");
-		}
+		}*/
 
 	}
 
